d0ad4665700b2a5794c238af21c9a33cbc6d1f2d
John Marshall <john@themillhousegroup.com>
2014-08-07 19:58:24 +1000

    Project basics in place - needs some Play deps to be sorted out next

--- build.sbt
+++ build.sbt
@@ -1,12 +1,19 @@
 name := "play2-reactivemongo-mocks"
 
-version := "0.11.0-SNAPSHOT"
+lazy val targetPlayReactiveMongoVersion = "0.11.0-SNAPSHOT"
+
+version := targetPlayReactiveMongoVersion
 
 organization := "com.themillhousegroup"
 
-val appDependencies = Seq(
+libraryDependencies ++= Seq(
     "org.mockito"         %   "mockito-all"           % "1.9.0",
-    "org.reactivemongo"   %%  "play2-reactivemongo"   % "0.11.0-SNAPSHOT"
+    "org.reactivemongo"   %%  "play2-reactivemongo"   % targetPlayReactiveMongoVersion
 )
+
+resolvers ++= Seq(  "oss-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+				    "oss-releases"  at "https://oss.sonatype.org/content/repositories/releases")
+
+jacoco.settings
 
 
---
+++ src/main/scala/com/themillhousegroup/reactivemongo/mocks/MongoMocks.scala
@@ -0,0 +1,72 @@
+package com.themillhousegroup.reactivemongo.mocks
+
+// Reactive Mongo imports
+
+import _root_.play.api.libs.json.JsObject
+import _root_.play.api.libs.json.JsValue
+import _root_.play.api.libs.json.Reads
+import _root_.play.api.libs.json.Writes
+import _root_.play.modules.reactivemongo.json.collection.JSONCollection
+import _root_.play.modules.reactivemongo.json.collection.JSONQueryBuilder
+import _root_.reactivemongo.api.CollectionProducer
+import _root_.reactivemongo.api.collections.GenericQueryBuilder
+import _root_.reactivemongo.api.DefaultDB
+import _root_.reactivemongo.api.FailoverStrategy
+import _root_.reactivemongo.core.commands.LastError
+import org.specs2.mock.Mockito
+import reactivemongo.api._
+import reactivemongo.api.collections.GenericQueryBuilder
+
+// Reactive Mongo plugin
+import play.modules.reactivemongo._
+import play.modules.reactivemongo.json.collection.{JSONQueryBuilder, JSONCollection}
+
+import org.specs2.mock.Mockito
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.libs.json._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.core.commands.{LastError, GetLastError}
+
+
+/**
+ * Still to do:  save, remove, update, uncheckedInsert, uncheckedRemove, uncheckedUpdate
+ */
+trait MongoMocks extends Mockito {
+  this: org.specs2.mutable.Specification =>
+
+  val mockDB = mock[DefaultDB]
+
+  /** Returns a mocked JSONCollection that can be used with the givenMongo... methods in MongoMocks */
+  def mockedCollection(name:String):JSONCollection = {
+    val mockCollection = mock[JSONCollection]
+    mockDB.collection[JSONCollection](org.mockito.Matchers.eq(name), any[FailoverStrategy])(any[CollectionProducer[JSONCollection]]) returns mockCollection
+    mockCollection
+  }
+
+  /* Requires the use of a Mockito spy, due to the Self-typing on sort(). */
+  private def givenMongoCollectionFindReturns(targetCollection:JSONCollection, result:Option[JsValue]) = {
+    val spiedQB = spy(new JSONQueryBuilder(targetCollection, mock[FailoverStrategy]))
+    org.mockito.Mockito.doReturn(Future.successful (result)).when(spiedQB).one[JsObject]
+    spiedQB.sort(any[JsObject]) returns spiedQB
+    targetCollection.find(any[JsObject])(any[Writes[JsObject]]) returns spiedQB
+  }
+
+  def givenMongoFindReturnsNothing(targetCollection:JSONCollection) = givenMongoCollectionFindReturns(targetCollection, None)
+  def givenMongoFindReturnsSome(targetCollection:JSONCollection, result:JsValue) = givenMongoCollectionFindReturns(targetCollection, Some(result))
+  def givenMongoFindReturns(targetCollection:JSONCollection, optResult:Option[JsValue]) = givenMongoCollectionFindReturns(targetCollection, optResult)
+
+
+  def givenMongoInsertIsOK(targetCollection:JSONCollection, ok:Boolean) = {
+    val mockResult = mock[LastError]
+    mockResult.ok returns ok
+    targetCollection.insert(any[JsObject])(any[ExecutionContext]) returns Future.successful(mockResult)
+  }
+
+  def queryResponse(response:Option[JsObject]) = {
+    val qb = mock[GenericQueryBuilder[JsObject, Reads, Writes]]
+    qb.one[JsObject] returns Future.successful(response)
+    qb
+  }
+}
